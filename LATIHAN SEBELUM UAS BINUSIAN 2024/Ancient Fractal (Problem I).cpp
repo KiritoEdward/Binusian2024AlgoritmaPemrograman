18fc20a91dbb772f0f3a0753fb9e6cbad7f0149a
EDWARD <60771749+KiritoEdward@users.noreply.github.com>
2021-02-07 09:48:50 +0700

    Update Ancient Fractal (Problem I).cpp

--- LATIHAN SEBELUM UAS BINUSIAN 2024/Ancient Fractal (Problem I).cpp
+++ LATIHAN SEBELUM UAS BINUSIAN 2024/Ancient Fractal (Problem I).cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    long long int x, y, i;
+    long long int awal, akhir, i;
     scanf("%lld %lld", &awal, &akhir);
     for(i = akhir; i <= akhir; i++) {
         printf("%lld %lld\n", i - ((i^(i-1)) >> 1), i);
